d4dbf3adda42ffa2c3395b1068f0929f091a0bb7
Jorge_Luis_Pena_Real <jorgeluispenareal@gmail.com>
2022-09-28 21:10:23 -0500

    added screen

--- src/navigator/navigator.tsx
+++ src/navigator/navigator.tsx
@@ -1,11 +1,17 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {LoginComponent} from '../components/login/LoginComponent';
+import {loginScreen} from '../screen/loginScreen';
+import {RegisterScreen} from '../screen/RegisterScreen';
 const Stack = createStackNavigator();
 export const MyStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="login" component={LoginComponent} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {backgroundColor: 'white'},
+      }}>
+      <Stack.Screen name="login" component={loginScreen} />
+      <Stack.Screen name="register" component={RegisterScreen} />
     </Stack.Navigator>
   );
 };
