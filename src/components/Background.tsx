4a7e585ca05cfa60aaafdfe5f247d1a85a4f5f7d
Jorge_Luis_Pena_Real <jorgeluispenareal@gmail.com>
2022-09-30 17:13:33 -0500

    added transfer

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
 
-export const Background = () => {
+export const Background: React.FC<{top?: number}> = ({top = -250}) => {
   return (
     <View
       style={{
         position: 'absolute',
         backgroundColor: '#5856D6',
-        top: -250,
+        top,
         width: 1000,
         height: 1200,
         transform: [{rotate: '-70deg'}],
--- src/screen/TransferScreen.tsx
+++ src/screen/TransferScreen.tsx
@@ -1,6 +1,7 @@
 import {useRoute} from '@react-navigation/native';
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import {
+  Button,
   KeyboardAvoidingView,
   Platform,
   StyleSheet,
@@ -11,6 +12,9 @@ import {useDispatch, useSelector} from 'react-redux';
 import {Background} from '../components/Background';
 import {getFruitDetails} from '../redux/fruits/fruitsActions';
 import {LoadingScreen} from './LoadingScreen';
+import {Picker} from '@react-native-picker/picker';
+import {ScrollView, TextInput} from 'react-native-gesture-handler';
+import RNPickerSelect from 'react-native-picker-select';
 
 export const TransferScreen: FC = (): JSX.Element => {
   const {params} = useRoute();
@@ -19,6 +23,14 @@ export const TransferScreen: FC = (): JSX.Element => {
   const fruit = useSelector((store: any) => store.fruits.fruit);
   const cityOrigin = useSelector((store: any) => store.cities.city);
   const dispatch = useDispatch();
+  const [selectedLanguage, setSelectedLanguage] = useState('java');
+
+  const [transfer, setTransfer] = useState({
+    origin: cityOrigin._id,
+    count: 0,
+    name: '',
+    destiny: '',
+  });
 
   useEffect(() => {
     const fetchFruit = async () => {
@@ -26,19 +38,67 @@ export const TransferScreen: FC = (): JSX.Element => {
     };
     fetchFruit();
   }, []);
+  const pickerRef = useRef<any>();
+
+  function open() {
+    pickerRef.current.focus();
+  }
+
+  function close() {
+    pickerRef.current.blur();
+  }
+  const onChangeNumber = (event: any) => {
+    setTransfer({
+      ...transfer,
+      count: Number(event),
+    });
+  };
+  const onValueChangePicker = (value: any) => {
+    setTransfer({
+      ...transfer,
+      destiny: value,
+    });
+  };
   return (
     <React.Fragment>
       <Background />
       <KeyboardAvoidingView style={{flex: 1}}>
         {fruit._id ? (
-          <View
-            style={{
-              ...styles.container,
-            }}>
-            <Text style={styles.title}> Choose an city to send </Text>
-            <Text style={styles.title}> {JSON.stringify(fruit)} </Text>
-            <Text style={styles.title}> {JSON.stringify(cityOrigin)} </Text>
-          </View>
+          <ScrollView>
+            <View
+              style={{
+                ...styles.container,
+              }}>
+              <Text style={{...styles.title}}>Choose a shop to send </Text>
+              <Text style={{...styles.title}}>
+                {' '}
+                {fruit.name} {fruit.count}{' '}
+              </Text>
+              <RNPickerSelect
+                style={pickerSelectStyles}
+                onValueChange={onValueChangePicker}
+                items={cities.map((e: any) => {
+                  return {label: e.name, value: e._id};
+                })}
+              />
+              <TextInput
+                style={styles.input}
+                onChangeText={onChangeNumber}
+                value={transfer.count.toString()}
+                placeholder="Count to send"
+                keyboardType="numeric"
+              />
+            </View>
+            <Button
+              disabled={!transfer.destiny}
+              title="Send Fruits"
+              color="#841584"
+              accessibilityLabel="Send Fruits"
+              onPress={() => {
+                console.log({transfer});
+              }}
+            />
+          </ScrollView>
         ) : (
           <LoadingScreen />
         )}
@@ -57,5 +117,34 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
     marginTop: 20,
+  },
+  input: {
+    backgroundColor: 'gray',
+    width: '100%',
+    marginTop: 35,
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    backgroundColor: 'lightyellow',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
   },
 });
