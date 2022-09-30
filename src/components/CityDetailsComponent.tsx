16cd743a99bb13345b07d388cc4fb67a16be9600
Jorge_Luis_Pena_Real <jorgeluispenareal@gmail.com>
2022-09-29 22:38:22 -0500

    added card

---
+++ src/components/CityDetailsComponent.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+import {ICity} from '../models/city.model';
+import {LoadingScreen} from '../screen/LoadingScreen';
+import {Background} from './Background';
+
+export const CityDetailsComponent: React.FC<{city: ICity}> = ({city}) => {
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={{
+        ...StyleSheet.absoluteFillObject,
+      }}>
+      <View
+        style={{
+          ...styles.container,
+          marginTop: 370,
+        }}>
+        <Text style={styles.title}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+          repellendus laudantium incidunt adipisci hic, perferendis mollitia,
+          magnam debitis iusto molestiae nihil! Quae amet saepe dolorum quod
+          iure unde inventore deleniti?
+        </Text>
+
+        <Text style={styles.title}>Fruits in this shop</Text>
+        <View style={{flexDirection: 'row'}}>
+          {city.fruits.map((ele, index) => (
+            <Text
+              style={{
+                ...styles.regularText,
+                marginRight: 10,
+                color: 'black',
+                borderColor: 'green',
+                borderWidth: 1,
+              }}
+              key={ele._id}>
+              {index + 1}.- {ele.name} {ele.count}
+            </Text>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: 'black',
+  },
+  regularText: {
+    fontSize: 19,
+  },
+  basicSprite: {
+    width: 100,
+    height: 100,
+  },
+});
--- src/screen/CityDetailsScreen.tsx
+++ src/screen/CityDetailsScreen.tsx
@@ -1,17 +1,28 @@
 import React, {useEffect} from 'react';
-import {Text, View, KeyboardAvoidingView, Platform} from 'react-native';
+import {
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+} from 'react-native';
 
 import {Background} from '../components/Background';
 import {useRoute} from '@react-navigation/native';
 import {getCityData} from '../redux/cities/citiesActions';
 import {useDispatch, useSelector} from 'react-redux';
 import {LoadingScreen} from './LoadingScreen';
+import {CityDetailsComponent} from '../components/CityDetailsComponent';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const CityDetailsScreen: React.FC<{}> = ({}) => {
   const {params} = useRoute();
   const {city}: any = params;
   const dispatch = useDispatch();
   const store = useSelector((e: any) => e.cities);
+  const {top} = useSafeAreaInsets();
 
   useEffect(() => {
     const fetchCity = async () => {
@@ -28,11 +39,70 @@ export const CityDetailsScreen: React.FC<{}> = ({}) => {
         {!store.city._id ? (
           <LoadingScreen />
         ) : (
-          <View style={{alignItems: 'center'}}>
-            <Text>{JSON.stringify(store.city)}</Text>
+          <View style={{flex: 1}}>
+            {/* Heade Containerr */}
+            <View
+              style={{
+                ...styles.headerContainer,
+                backgroundColor: 'lightblue',
+              }}>
+              <Text
+                style={{
+                  ...styles.pokemonName,
+                  top: top + 40,
+                }}>
+                Shop: {store.city.name + '\n'}
+              </Text>
+              <Image source={{uri: store.city.image}} style={styles.pokeball} />
+            </View>
+            {!store.city.name ? (
+              <View style={styles.loadingIndicator}>
+                <ActivityIndicator color={'red'} size={50} />
+              </View>
+            ) : (
+              <CityDetailsComponent city={store.city} />
+            )}
           </View>
         )}
       </KeyboardAvoidingView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 370,
+    zIndex: 999,
+    alignItems: 'center',
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000,
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+  },
+  pokemonName: {
+    color: 'white',
+    fontSize: 40,
+    alignSelf: 'flex-start',
+    left: 20,
+  },
+  pokeball: {
+    width: 250,
+    height: 250,
+    bottom: -20,
+    opacity: 0.7,
+    borderRadius: 55,
+  },
+  pokemonImage: {
+    width: 250,
+    height: 250,
+    position: 'absolute',
+    bottom: -15,
+  },
+  loadingIndicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
